1e0032fb08a0993c2830b89d430b73316162a81b
cannotilever <cannotilever@protonmail.com>
2023-07-11 21:09:15 -0400

    update auth methods in plist, add runme file

---
+++ runme.sh
@@ -0,0 +1,24 @@
+echo "Getting ready to install Google LDAP."
+echo "I'm going to need elevated privileges. Please enter your sudo password when prompted"
+echo "\n"
+echo "Downloading Python 3..."
+curl -O https://www.python.org/ftp/python/3.11.4/python-3.11.4-macos11.pkg
+echo "\n Done. Installing Python 3..."
+sudo installer -pkg /Volumes/LDAP/python-3.10.6-macos11.pkg -target /
+echo "Installing libraries..."
+pip3 install pyobjc-framework-OpenDirectory
+if [ $? -ne 0 ] ; then
+    echo "pip libary install failed, please try again manually"
+    exit 1
+fi
+echo "Running python script"
+source ~/.zshrc
+sudo python3 main22.pyobjc
+if [ $? -eq 127 ] ; then
+    echo "looks like the python3 install failed"
+    exit 1
+elif [ $? -ne 0 ] ; then
+    echo "Sorry, something went wrong with the python script :("
+    exit 1
+fi
+# we don't need a success condition because if successful, the python script reboots the computer.
